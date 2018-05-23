4f63206b9b2e2114f7b51cecc50168620a89a1fd
Oya <marciooya@gmail.com>
2018-05-23 00:44:42 -0300

    insta

--- js/nuovi.plugins.js
+++ js/nuovi.plugins.js
@@ -166,9 +166,9 @@
 						limit: 5,
 						get: 'user',
 						userId: 648355,
-						accessToken: '648355.7e70bd0.4f91b3735bd34157ba112f56edffcb26',
+						accessToken: '2869726471.b8d5acd.25785fb5d9194c44857e3ff6614ba51e',
 						resolution: 'standard_resolution',
-						clientId: '7e70bd0c50b44b60b3dd0ae869d18c40',
+						clientId: 'b8d5acd2a03f4cb5a47c6415069ea673',
 						template: '<div class="nv-col"><div class="nv-instafeed-item"><a class="fancybox nv-lightbox" rel="instagram" href="{{image}}" title="{{location}}"><img src="{{image}}" /></a></div></div>',
 						after: function(){
 							$('#' + this.options.target).find('.fancybox').fancybox();
